8f043a7751c29bbbb108038ec9cf3f274bb8ae1d
David Forero <davefogo@gmail.com>
2016-10-01 19:21:51 -0500

    assignment 22: complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,12 @@
-var animatePoints = function(){
+var points = document.getElementsByClassName('point');
 
-          var points = document.getElementsByClassName('point');
+function revealPoint(array){
+  for (var i = 0; i < array.length; i++) {
+    array[i].style.opacity = 1;
+    array[i].style.transform = "scaleX(1) translateY(0)";
+    array[i].style.msTransform = "scaleX(1) translateY(0)";
+    array[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
+};
 
-          var revealFirstPoint = function() {
-              points[0].style.opacity = 1;
-              points[0].style.transform = "scaleX(1) translateY(0)";
-              points[0].style.msTransform = "scaleX(1) translateY(0)";
-              points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-          };
-
-          var revealSecondPoint = function() {
-              points[1].style.opacity = 1;
-              points[1].style.transform = "scaleX(1) translateY(0)";
-              points[1].style.msTransform = "scaleX(1) translateY(0)";
-              points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-          };
-
-          var revealThirdPoint = function() {
-              points[2].style.opacity = 1;
-              points[2].style.transform = "scaleX(1) translateY(0)";
-              points[2].style.msTransform = "scaleX(1) translateY(0)";
-              points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-          };
-
-          revealFirstPoint();
-          revealSecondPoint();
-          revealThirdPoint();
-      };
-
-      animatePoints();
+revealPoint(points);
