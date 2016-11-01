20ae07c52075f10ba54eeaac5fed316596a35ee3
David Forero <davefogo@gmail.com>
2016-11-01 16:38:10 -0500

    assignment 27: complete

--- scripts/album.js
+++ scripts/album.js
@@ -65,13 +65,13 @@ var findParentByClassName = function(element, targetClass) {
     if (currentParent == null) {
       alert('No parent found')
     }
-    while (currentParent.className != targetClass) {
+    while (currentParent.className != targetClass && currentParent.className !== null) {
       currentParent = currentParent.parentElement;
     }
-    return currentParent;
     if (currentParent == null) {
       alert('No parent found with that class name')
     }
+    return currentParent;
   }
 };
 
